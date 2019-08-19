fdd8bed99bc6c9f5da3d025e5215fa7d32f51a7d
Coin <cce5784@gmail.com>
2019-08-19 20:23:08 +0800

    Add WrapContentListView

---
+++ library/src/main/kotlin/jahirfiquitiva/libs/frames/ui/widgets/WrapContentListView.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019. Jahir Fiquitiva
+ *
+ * Licensed under the CreativeCommons Attribution-ShareAlike
+ * 4.0 International License. You may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *    http://creativecommons.org/licenses/by-sa/4.0/legalcode
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jahirfiquitiva.libs.frames.ui.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ListView
+
+class WrapContentListView : ListView {
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context) : super(context)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE shr 2,
+                MeasureSpec.AT_MOST)
+        super.onMeasure(widthMeasureSpec, expandSpec)
+    }
+}
